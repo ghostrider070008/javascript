cb101c1277864cf646d69924a0ad5aee3de9ae46
ghostrider070008 <46030854+ghostrider070008@users.noreply.github.com>
2018-12-20 22:08:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var tc=+prompt("Введите температуру по Цельсию");
+var tf=(9/5)*tc+32;
+alert("Температура по Фаренгейту="+tf);
+var admin, name;
+name="Василий";
+admin=name;
+alert(admin);
+var a=1000, b="108", c=a+b;
+alert(c);
+
