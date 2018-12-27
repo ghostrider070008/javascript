be974e9c29f4b597d2cf701ea9a15b3e3681de1f
ghostrider07008 <ghostrider07008@gmail.com>
2018-12-27 16:59:17 +0300

    Продолжение следует

--- 1.js
+++ 1.js
@@ -1,12 +1,22 @@
 var i=2, j=1, n=0;
 while (i<=100){
+	n=0;
 	if (i%2!=0){
+		j=1;
 		while(j<=100){
 			if (n<=2){
-				if (i%j==0)
+				if (i%j==0){
+					console.log(i);
 					n++;
-				
+					j++;				
+				}
+			if (n>2){
+			break;
+			}
+			
 			}
 		}
 }
+
+i++;
 }
