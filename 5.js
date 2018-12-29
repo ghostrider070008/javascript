2bc148f4d349e80b476c45de6722318816645bac
ghostrider07008 <ghostrider07008@gmail.com>
2018-12-29 12:27:56 +0300

    Генерация 4 разных цифр в компьютере

--- 5.js
+++ 5.js
@@ -1,7 +1,54 @@
-var x,p,k,i=0,j=1,n=[];
-x=prompt("Введите число из неповторяющихся цифр");
-x+=x+"";
-while (j<4){
-n[i]=Math.random()*10;
+var x,p,k,i=0,j=0,n=[],t=1;
+/*x=prompt("Введите число из неповторяющихся цифр");
+x+=x+"";*/
+n[i]=Math.round(Math.random()*9+0);
+if (n[i]==0)
+n[i]=Math.round(Math.random()*9+0);
+console.log(n[i]);
+i++;
+while (i<4){
+	while (t=1){
+		
+ x=Math.round(Math.random()*9+0);
+if (n.indexOf(x)==-1){
+	n[i]=x;
+	break;
+}
 
-}	
+	}
+console.log(n[i]);
+	i++;
+
+}
+
+
+
+/*while (i<4){
+n[i]=Math.round(Math.random()*8+1);
+j=0;
+
+	while (j<=n.length-1){
+	if ((n[i]==n[j]) && (j!=n.length) && (i!=j)){
+	n[i]=Math.round(Math.random()*8+1);
+	if (n[i]==n[j]){
+	n[i]=Math.round(Math.random()*8+1);
+	}
+	k=0;
+	while (k<=n.length-1){
+		if ((n[i]==n[k]) && (k!=n.length) && (i!=k)){
+		n[i]=Math.round(Math.random()*8+1);
+	if (n[i]==n[k]){
+	n[i]=Math.round(Math.random()*8+1);
+	}
+		}
+		k++;
+	}
+		}
+	j++;
+	}
+console.log(n[i]);
+i++;
+}
+i=0;
+j=i+1;
+k=0;	*/
