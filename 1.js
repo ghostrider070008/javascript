dfce7b3c6acdbedf7e42eccee341b478bddbaa1d
ghostrider070008 <ghostrider07008@gmail.com>
2019-01-05 16:53:46 +0300

    начало

--- 1.js
+++ 1.js
@@ -1,22 +1,7 @@
-var n=2, i,j;
-var table="<table>", tr=1,temp,td;
-table+="<tr>"+"<td>"+n+"</td>";
-n++;
-while (n<=100) {
-	i=2;
-	j=0;
-	if (n%2!=0){
-	while ((i*i<=n)&&(j!=1)){
-	if (n%i==0){
-		j=1;
-	}
-	i++;
-	}
-	if (j==0){
-		table+="<td>"+n+"</td>";
-		}
-	}
-	n++;
-}
-table+="</tr></table>";
-document.write(table);
+var chislo={
+	sotni: "",
+	desyatki: "",
+	edinicy: ""
+	
+};
+function preobr
