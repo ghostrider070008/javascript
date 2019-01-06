7c3563f3bfca80ddcad2cb0ad89144e6b8571001
ghostrider070008 <ghostrider07008@gmail.com>
2019-01-06 15:41:29 +0300

    3

--- 1 Задание/1.js
+++ 1 Задание/1.js
@@ -1,15 +1,18 @@
 var number = {
+	//единицы
 		units: function (my_numberfun ) {
 	this.units = my_numberfun % 10;
 },
+//десятки
 	decades: function (my_numberfun ) {
 	this.decades = parseInt((my_numberfun/10) % 10);
 },
+//сотни
 	hundreds: function (my_numberfun ) {
 	this.hundreds = parseInt((my_numberfun/100) % 10);
 }
 }
-var my_input=+prompt("Введите число от 0 до 999:");
+var my_input=+prompt("Введите число от 0 до 999:");//ввод числа
 /*while ((my_input<0) || (my_input>999)){
 	alert("Число должно находиться в промежутке от 0 до 999");
 	my_input=+prompt("Введите число от 0 до 999:");
