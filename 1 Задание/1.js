71b7c9ee94d0b39bb016a496333de8725cf0a767
ghostrider070008 <ghostrider07008@gmail.com>
2019-01-06 13:57:01 +0300

    первое готово

--- 1 Задание/1.js
+++ 1 Задание/1.js
@@ -1,20 +1,27 @@
 var number = {
-	hundreds: function (my_numberfun ) {
-	this.hundreds = parseInt((my_numberfun/100) % 10);
+		units: function (my_numberfun ) {
+	this.units = my_numberfun % 10;
 },
 	decades: function (my_numberfun ) {
 	this.decades = parseInt((my_numberfun/10) % 10);
 },
-	units: function (my_numberfun ) {
-	this.units = my_numberfun % 10;
+	hundreds: function (my_numberfun ) {
+	this.hundreds = parseInt((my_numberfun/100) % 10);
 }
-};
+}
 var my_input=+prompt("Введите число от 0 до 999:");
-while ((my_input<0) || (my_input>999)){
+/*while ((my_input<0) || (my_input>999)){
 	alert("Число должно находиться в промежутке от 0 до 999");
 	my_input=+prompt("Введите число от 0 до 999:");
-};
-number.hundreds(my_input);
-number.decades(my_input);
+};*/
+if ((my_input >0) && (my_input<=999)){ 
 number.units(my_input);
-console.log(number);
+number.decades(my_input);
+number.hundreds(my_input);
+console.log("\'единицы\' : "+number.units+", \'десятки\' : "+number.decades+", \'сотни\' : "+number.hundreds);
+}
+else {
+	console.log('Число не входит в заданный диапазон от 0 до 999');
+	number={};
+	console.log(number);
+}
