8f624cbf9fd2fea797760b5b6e27d92aa684de7b
ghostrider070008 <ghostrider07008@gmail.com>
2019-01-07 21:16:57 +0300

    Сделал 3 задание

--- 3 Задание/gameCode_millionaire1.js
+++ 3 Задание/gameCode_millionaire1.js
@@ -1,17 +1,29 @@
 var answer, ok, correct, correct_str;
 q_num=0;
 correct=true;
+alert("Приветствуем тебя, Дорогой друг на игре: \n \"Кто хочет стать миллионером!\"")
+
+//цикл Вопросов
 for (i=1; ((i<=15) && (correct=true)); i++) {
-conclusionQuestion(q_num);
+	do{
+ok = conclusionQuestion(q_num);
+	} while (ok == false);
 correct = qwestionsChek(answer);
 if (correct == false){
-	alert ('Вы закончили игру с выигрышом в 0 рублей\n Правильный ответ на вопрос был: '+qwestions[q_num].correct+"Спасибо за игру!");
-}
-/*
+	alert ('Вы закончили игру с выигрышом в 0 рублей\n Правильный ответ на вопрос был: \n'+qwestions[q_num].correct_answer+"\nСпасибо за игру!");
+	break;
+	}
+	
 if (answer == -1) {
-		alert('Вы закончили игру с выигрышом в 0 рублей');
-		i=16;
-}*/
+		break;
+}
+if (answer == -2) {
+		break;
+}
+if (q_num == 15){
+	alert('Поздравляем Вы выиграли максимальную сумму в 3 000 000 рублей \n Спасибо за игру!\n Вы просто гений!')
+	break;
+}
 }
 
 //функция проверки ответа
@@ -24,7 +36,7 @@ function qwestionsChek(answer){
 	}
 	if ((answer>0) && (answer<5) && (answer!=qwestions[q_num].correct)) {
 		correct = false;
-		i=16;
+		//i=16;
 		return correct;
 	}
 }
@@ -34,12 +46,16 @@ function conclusionQuestion(q_num){
 	//answer = +prompt("Сумма вопроса:"+qwestions[q_num].summa+"\n"+qwestions[q_num].text_q+"\n"+qwestions[q_num].a1+"\n"+qwestions[q_num].a2+"\n"+qwestions[q_num].a3+"\n"+qwestions[q_num].a4);
 	do {
 	ok=false;
-	answer = +prompt("Сумма вопроса:"+qwestions[q_num].summa+"\n"+qwestions[q_num].text_q+"\n"+qwestions[q_num].a1+"\n"+qwestions[q_num].a2+"\n"+qwestions[q_num].a3+"\n"+qwestions[q_num].a4);
+	answer = +prompt("Вопрос на : "+qwestions[q_num].summa+' рублей'+"\n"+qwestions[q_num].text_q+"\n"+qwestions[q_num].a1+"\n"+qwestions[q_num].a2+"\n"+qwestions[q_num].a3+"\n"+qwestions[q_num].a4);
 	//answer=+prompt(qwestions[1].text_q2+"\n"+qwestions[1].a1+"\n"+qwestions[1].a2+"\n"+qwestions[1].a3+"\n"+qwestions[1].a4);
 	if (answer == -1) {
 		alert('Вы закончили игру с выигрышом в 0 рублей');
 		break;
 	}
+	if (answer == -2) {
+		alert('Вы закончили игру с выигрышом в '+ qwestions[q_num-1].summa+' рублей');
+		break;
+}	
 	else {
 		ok = isAnswer(4, answer);
 	}
