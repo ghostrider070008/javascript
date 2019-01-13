cbbff56349e063c93a4c6240c3a0a28dfed5599a
ghostrider070008 <ghostrider07008@gmail.com>
2019-01-13 10:19:37 +0300

    изменил стили изображения

--- 1 Задание/script.js
+++ 1 Задание/script.js
@@ -1,9 +1,10 @@
 function generationImg(){
 var images = document.createElement('img');
+images.className = "img_style";
 images.alt = "изображение 1";
 images.src = Image[0].src_img;
-images.width = "300px";
-images.height = "300px";
+images.style.width = "400px";
+images.style.height = "300px";
 conteiner.appendChild(images);
 console.log(images);
 }
