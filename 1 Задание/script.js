d92db12dfa0f88d5b99c996bee1c07f1d22dbef7
ghostrider070008 <ghostrider07008@gmail.com>
2019-01-14 22:27:55 +0300

    Решено первое задание.

--- 1 Задание/script.js
+++ 1 Задание/script.js
@@ -1,5 +1,5 @@
 var div_clear, image_max;
-
+//функция генерации изображения основная
 function generationImg(){
 var i = 0, images;
 
@@ -12,26 +12,40 @@ var img_click = document.createElement('img');
 	img_click.src = '';
 	conteiner.appendChild(img_click);
 }
-
+// функция генерация маленьких изображений
 function generationImgMin(Image, i){
-	images = document.createElement('img');
+	var images = document.createElement('img');
 	images.className = "img_style";
 	images.id = i;
 	images.alt = Image[i].alt;
-	images.onclick = function (){
-	var	img_click = document.getElementsByClassName("img_max_style");
-		img_click[0].remove();
-	img_click = document.createElement('img');
-		img_click.className = "img_max_style";
-		img_click.alt = Image[i].alt;
-		img_click.src = Image[i].src_img;
-		conteiner.appendChild(img_click);
-		}
 	images.src = Image[i].src_img;
 	conteiner.appendChild(images);
 	console.log(images);
+	//обработчик собития "клика"
+	images.onclick = function (){
+	 var img_click = document.getElementsByClassName("img_max_style");
+	 if (img_click.length !=0) {
+		img_click[0].remove();
+	 }
+	img_click = document.createElement('img');
+		img_click.className = "img_max_style";
+		img_click.alt = Image_1[i].alt;
+		img_click.src = Image_1[i].src_img;
+		conteiner.appendChild(img_click);
+		//обработчик события ошибки (если изображение отсутствует)
+		img_click.onerror = function() {
+		var	img_click_1 = document.getElementsByClassName("img_max_style");
+		if (img_click_1.length !=0) {
+		img_click_1[0].remove();
+		}
+		alert("Изображение не найдено");
+		}
+	
+		//document.getElementsByClassName("img_max_style").textContent = "Изображение отсутствует!";
+	}
+	
 }
-
+/*
 function generationDivImage(){
 	var div_clear, image_max;
 	//div_clear = document.createElement('div');
@@ -45,4 +59,4 @@ function generationDivImage(){
 		img_click.src = Image[0].src_img;
 		console.log(img_click);
 	}
-}
+}*/
--- 1 Задание/text.js
+++ 1 Задание/text.js
@@ -2,4 +2,10 @@ var Image = [
 {src_img : "img/img_1.jpg", alt : "Изображение_1"},
 {src_img : "img/img_2.jpg", alt : "Изображение_2"},
 {src_img : "img/img_3.jpg", alt : "Изображение_3"},
+], Image_1 = [
+{src_img : "img/img_1.jpg", alt : "Изображение_1"},
+{src_img : "img/img_2.jpg", alt : "Изображение_2"},
+{//src_img : "img/img_3.jpg", alt : "Изображение_3"
+},
 ];
+
