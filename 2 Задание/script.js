eb3d989e9472b99e649fffd7ae3191299e3149f1
ghostrider070008 <ghostrider07008@gmail.com>
2019-01-16 01:17:12 +0300

    почти сделал

--- 2 Задание/script.js
+++ 2 Задание/script.js
@@ -1,8 +1,90 @@
-function f(id){
+function f(id_prod){
 var product1 = document.getElementsByClassName("car");
-var productName1 = document.createElement("h3");
+	var	productId = "productId"+id_prod;
+	if (document.getElementById (productId) != null){
+		var colprod = document.getElementById(colprodID);
+		var colvo = colprod.textContent;
+		productCar[id_prod].col++;
+		colprod.innerHTML = productCar[id_prod].col;
+		//colprod.textContent = productCar[id_prod].col;
+		//colprod.remove();
+		var trprod = document.getElementById(idstr);
+		price = document.getElementById(priceid);
+		sumid = "sum"+id_prod;
+	var	producttdsum = document.getElementById(sumid)
+		sum = product[id_prod].price*productCar[id_prod].col;
+	producttdsum.innerHTML = sum;
+	totalSum += productCar[id_prod].price*productCar[id_prod].col;
+	totaltd = document.getElementById("tdtotalsum");
+	totaltd.innerHTML = totalSum;
+	
+		//trprod.insertBefore(colprod,price);
+	}
+	if (document.getElementById (productId) == null){
+	var productprov = document.getElementById(productId);
+var productTr = document.createElement("tr"), productTdName = document.createElement("td");
+var cartable = document.getElementsByTagName("table");
+	productTdName.id = productId;
+	idstr = "tr"+ id_prod;
+	productTr.id = "tr"+ id_prod;
+	//productTdName.textContent = product[id_prod].name;
+	productTdName.innerHTML = product[id_prod].name;
+	productTr.appendChild(productTdName);
+var	productTdcol = document.createElement("td");
+	col++;
+	productTdcol.id = "col"+id_prod;
+	colprodID = "col";
+	colprodID+=id_prod;
+	productCar[id_prod].col++;
+	productTdcol.textContent = productCar[id_prod].col;
+	productTr.appendChild(productTdcol);
+	
+	productTdName = document.createElement("td");
+	priceid = "priceId"+id_prod;
+	productTdName.id = priceid;
+	productTdName.textContent = product[id_prod].price;
+	productTr.appendChild(productTdName);
+	productTdName = document.createElement("td");
+	sumid = "sum"+id_prod;
+	productTdName.id = "sum"+id_prod;
+var sum = product[id_prod].price*productCar[id_prod].col;
+	productTdName.innerHTML = sum;
+	productTr.appendChild(productTdName);
+	console.log(productTr);
+	console.log(cartable);
+	console.log(productprov);
+	cartable[0].appendChild(productTr);
+	//итог
+	if (document.getElementById("tdtotalsum") != null){
+	totalSum += productCar[id_prod].price*productCar[id_prod].col;
+	totaltd = document.getElementById("tdtotalsum");
+	totaltd.innerHTML = totalSum;
+	}
+	if (document.getElementById("tdtotalsum") == null){
+	totaltr = document.createElement("tr");
+	totaltr.id = "total";
+	totaltr.appendChild(document.createElement("td"));
+	totaltd = document.createElement("td");
+	totaltd.innerHTML = "Итого:";
+	totaltr.appendChild(totaltd);
+	totaltr.appendChild(document.createElement("td"));
+		totalSum += productCar[id_prod].price*productCar[id_prod].col;
+	totaltd = document.createElement("td");
+	totaltd.id = "tdtotalsum";
+	totaltd.innerHTML = totalSum;
+	totaltr.appendChild(totaltd);
+	cartable[0].appendChild(totaltr);
+	console.log(productCar);
+	}
+	
+	}
+	
+	
+
+/*productName1 = document.createElement("h3");
+productName1.className = "nametovarCar";
 productName1.textContent = product[id].name;
-product1[0].appendChild(productName1);
+product1[0].appendChild(productName1);*/
 }
 /*
 var div_clear, image_max;
--- 2 Задание/text.js
+++ 2 Задание/text.js
@@ -2,5 +2,10 @@ var product = [
 {src_product : "img/ball.jpg", alt : "Футболный мяч", name : "Официальный игровой мяч 2018 FIFA", price: 3000},
 {src_product : "img/puzzle.jpg", alt : "3D-пазл", name : "3D-пазл стадиона Камп Ноу", price: 1800},
 {src_product : "img/brelok.jpg", alt : "Брелок Арсенал", name : "Брелок Арсенал 1892 овальный", price: 300},
-];
+], col = 0, colprodID, priceid,idstr,
+productCar = [
+{ name : "Официальный игровой мяч 2018 FIFA", price: 3000, col: 0},
+{ name : "3D-пазл стадиона Камп Ноу", price: 1800, col: 0},
+{ name : "Брелок Арсенал 1892 овальный", price: 300, col: 0}
+], totalSum = 0,sumid,totaltr,totaltd;
 
